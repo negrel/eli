8c862ead6721173bdd73b6bf0d9b2b4f76c64ca2
Alexandre Negrel <negrel.dev@protonmail.com>
2022-05-27 17:17:37 +0200

    update lib/log to prevent log fn spam in trace

--- distributions/.common/lib/log.sh
+++ distributions/.common/lib/log.sh
@@ -28,12 +28,18 @@ function log_pipe {
 }
 
 function _log_level {
+  set +x
+
   local color="$1"
   local level="$2"
   shift 2
 
   printf "\033[${log_colors[$color]}m" 1>&2
   log "[$ELI_LOG_PREFIX] [%s] - \033[0m%s" "$level" "$(printf "%s " $@)"
+
+  if [ -n "$ELI_TRACE" ]; then
+    set -x
+  fi
 }
 
 function log_debug {
@@ -68,6 +74,14 @@ function stacktrace {
   local frame=0 LINE SUB FILE
   while read LINE SUB FILE < <(caller "$frame"); do
     printf "\t\033[${log_colors[red]}m%s()\n\t\t%s:%s\033[0m\n" "$SUB" "$FILE" "$LINE" 1>&2
+    ((frame++))
+  done
+}
+
+function stacktrace {
+  local frame=0 LINE SUB FILE
+  while read LINE SUB FILE < <(caller "$frame"); do
+    echo "$SUB" "$FILE" "$LINE" 1>&2
     ((frame++))
   done
 }
