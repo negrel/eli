858b256cf0a8900e5643e27ae732afe992a78d62
Alexandre Negrel <negrel.dev@protonmail.com>
2022-06-28 17:35:15 +0200

    add pre install hooks

--- distributions/.common/hooks/pre-install/mkswap.sh
+++ distributions/.common/hooks/pre-install/mkswap.sh
@@ -14,17 +14,17 @@ if [ "$ELI_INSTALL_TYPE" = "iso" ]; then
   exit 0
 fi
 
-if [ -z "$ELI_SWAP_ID" ]; then
+if [ -z "$ELI_SWAP_DEVICE" ]; then
   log_warn "no swap id specified, skipping swap device preparation"
   exit 0
 fi
 
 log_info "setting up swap device..."
 
-mkswap "$ELI_SWAP_ID" 
+mkswap "$ELI_SWAP_DEVICE"
 
-log_debug "adding $ELI_SWAP_ID swap entry to fstab..."
-echo "$ELI_SWAP_ID	none	swap	sw	0	0" >> /etc/fstab
-log_debug "$ELI_SWAP_ID swap entry added to fstab."
+log_debug "adding $ELI_SWAP_DEVICE swap entry to fstab..."
+echo "$ELI_SWAP_DEVICE	none	swap	sw	0	0" >> /etc/fstab
+log_debug "$ELI_SWAP_DEVICE swap entry added to fstab."
 
 log_info "swap device successfully setted up."
